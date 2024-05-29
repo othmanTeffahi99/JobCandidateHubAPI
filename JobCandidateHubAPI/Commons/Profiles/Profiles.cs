991d9c2d51c01ef557f03a54a2005dd08540f858
othmane teffahi <othmane.teffahi@outlook.com>
2024-05-29 12:54:13 +0100

    +ADD: Profiles.cs for mapping Entities and Dtos

---
+++ JobCandidateHubAPI/Commons/Profiles/Profiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using JobCandidateHubAPI.Dtos;
+using JobCandidateHubAPI.Entities;
+
+namespace JobCandidateHubAPI.Commons.Profiles;
+
+public class Profiles : Profile
+{
+    public Profiles()
+    {
+        CreateMap<Candidate, CandidateReadDto>();
+        CreateMap<CandidateReadOrUpdateDto, Candidate>();
+    }
+}
